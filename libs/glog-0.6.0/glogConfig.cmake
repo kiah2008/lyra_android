1e7e91a0ead952c5f465b45cc30d0e1d4d9e5bfa
zhao kai <zhaokai@megvii.com>
2022-08-10 08:44:44 +0800

    update liblyra

--- libs/glog-0.6.0/glogConfig.cmake
+++ libs/glog-0.6.0/glogConfig.cmake
@@ -9,13 +9,10 @@ target_include_directories(
 
 file(GLOB USER_LIBS_PATH ${CMAKE_CURRENT_LIST_DIR}/${ANDROID_ABI}/*.a)
 foreach (f ${USER_LIBS_PATH})
-    message("file : ${f}")
     get_filename_component(filename ${f} NAME)
     string(LENGTH "${filename}" len)
-    message("filename: ${filename} , len: ${len}")
     math(EXPR sublen ${len}-3-2)
     string(SUBSTRING "${filename}" 3 ${sublen} libname)
-    message("abseil libname : ${libname}")
     add_library(${libname} STATIC IMPORTED )
     set_target_properties(${libname} PROPERTIES
         IMPORTED_LOCATION "${f}")
